ed776a902a76d6dce527cb22e9cc80a5580edec4
Murat Yaşar <127419303+murat-yasar@users.noreply.github.com>
2024-03-24 22:03:57 +0100

    Add 13. Roman to Integer

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,8 @@ twoSum([3,2,4], 6);     // Output: [1,2]
 twoSum([3,3], 6);       // Output: [0,1]
 
 
+----------------------------------------------------------------------------------------------------
+
 //* 9. Palindrome Number
 /** 
  * Given an integer x, return true if x is a palindrome, and false otherwise.
@@ -49,3 +51,47 @@ console.log("9. Palindrome Number");
 isPalindrome(121);  // Output: true
 isPalindrome(-121); // Output: false
 isPalindrome(10);   // Output: false
+
+
+----------------------------------------------------------------------------------------------------
+
+//* 13. Roman to Integer
+/** 
+ * M=1000, D=500, C=100, L=50, X=10, V=5, I=1
+ * Given a roman numeral, convert it to an integer.
+ * 
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(str) {
+    let result = 0, intArr = [];
+
+    for (let i in str){
+      switch (str[i]) {
+        case "M": intArr.push(1000);  break;
+        case "D": intArr.push(500);   break;
+        case "C": intArr.push(100);   break;
+        case "L": intArr.push(50);    break;
+        case "X": intArr.push(10);    break;
+        case "V": intArr.push(5);     break;
+        case "I": intArr.push(1);     break;
+      }
+    }
+
+    intArr.push(0); // To fix the problem of checking the last character
+    for (let i=0; i<intArr.length; i++){
+      (intArr[i] >= intArr[i+1]) ? result += intArr[i] : result -= intArr[i];
+    }
+
+    return result;
+};
+
+// TEST CASES
+console.log("13. Roman to Integer");
+romanToInt("III");      // Output: 3
+romanToInt("LVIII");    // Output: 58
+romanToInt("MCMXCIV");  // Output: 1994
+
+
+----------------------------------------------------------------------------------------------------
+
